fc1b95c1c314766c84961a10ed1dd60c1e69ff4c
develop <bumrungsak.s@hotmail.com>
2019-01-07 11:25:18 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -2,9 +2,11 @@
 
 require "vendor/autoload.php";
 
-$access_token = 'ahbj8DK/OxNGHqr9fwgt4pNzw2TNhNIBtmbvEBw0ILtArrE0S78BE+FVulEvtk3y62bz7QkbT+J0f5MJRlC6yMRv4IQyMyTRQLEBV8s+UI/sdJ5YiKkhLB/mmgU8JhoIByJcUoH06CYb3hUotv6BFwdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = 'd0271fd54d3126540bdb2d831101ccd3';
+$access_token = '4uSfwkeFBaSmEoRdsABiJlL5l8jHYcYU/p+6pGwly6szAcQNzB/GtUOnKtnpC4shzdJ+c8A30qTS3sF576t0pJR16Qv/o6LeLJgRVBxdS3c6a/A+FTwqWqWKMsLRNPWjwo8v1KOQCJqH0fHFwgDgYwdB04t89/1O/w1cDnyilFU=';
+
+
+$channelSecret = 'be71673fa018908c8928cb5a187a1714';
 
 $pushID = 'Ue8397db755c9256de543ed18d01dff28'; // my ID line
 
